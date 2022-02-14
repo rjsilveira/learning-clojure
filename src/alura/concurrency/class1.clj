f3af45d9a6e47703fb57fce7fd5aaaf13a97be63
Raphael Silveira <raphael.silveira@raphael.silveira>
2022-02-14 15:56:56 -0300

    Thread

--- src/alura/concurrency/class1.clj
+++ src/alura/concurrency/class1.clj
@@ -10,6 +10,7 @@
   (def hospital (a.c.concurrency_logic/coming-in hospital :waiting-room "Fernanda"))
   (def hospital (a.c.concurrency_logic/coming-in hospital :waiting-room "Larissa"))
   (def hospital (a.c.concurrency_logic/coming-in hospital :waiting-room "Riquelme"))
+  (def hospital (a.c.concurrency_logic/coming-in hospital :waiting-room "Adriano"))
   (pprint hospital)
   (def hospital (a.c.concurrency_logic/coming-in hospital :lab-01 "Fernanda"))
   (def hospital (a.c.concurrency_logic/coming-in hospital :lab-03 "Larissa"))
@@ -18,3 +19,34 @@
   (pprint hospital))
 
 (day-one)
+
+(defn coming-in-function
+  "Coming in function"
+  [person]
+  (def hospital (a.c.concurrency_logic/coming-in hospital :waiting-room person)))
+
+(defn coming-in-function-with-pause
+  "Coming in function"
+  [person]
+  (Thread/sleep (* (rand) 2000))
+  (def hospital (a.c.concurrency_logic/coming-in hospital :waiting-room person)))
+
+(defn parallel
+  "Parallel"
+  []
+  (def hospital (a.c.model/create-hospital))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Miranda"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Rogério"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Igor"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Diego"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Reinaldo"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Luan"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Gabriel"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Marcos"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Éder"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Pablo"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Luciano"))))
+  (.start (Thread. (fn [] (coming-in-function-with-pause "Rafael"))))
+  (.start (Thread. (fn [] (Thread/sleep 4000) (pprint hospital)))))
+
+(parallel)
--- src/alura/concurrency/concurrency_logic.clj
+++ src/alura/concurrency/concurrency_logic.clj
@@ -1,9 +1,19 @@
 (ns alura.concurrency.concurrency_logic)
 
+(defn full?
+  "The queue is full?"
+  [hospital department]
+  (-> hospital
+      (get,,, department)
+      count,,,
+      (<,,, 5)))
+
 (defn coming-in
   "Coming in"
   [hospital department person]
-  (update hospital department conj person))
+  (if (full? hospital department)
+    (update hospital department conj person)
+    (throw (ex-info "Queue is full!" {:person person}))))
 
 (defn attend
   "Attend a person"
